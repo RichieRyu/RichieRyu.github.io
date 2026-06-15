c6f647a06c19da6f5b4de366b8ccbde79503ed3d
RichieRyu <160171987+RichieRyu@users.noreply.github.com>
2026-06-15 19:10:31 +0000

    Deploying to gh-pages from @ RichieRyu/RichieRyu.github.io@a70191ecd007adee8a4f3d95248a4b7def010220 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -36,6 +36,9 @@ ninja.data = [{
           section: "News",},{id: "news-distributed-perception-aware-safe-leader-follower-system-via-control-barrier-methods-with-tony-tran-miao-pan-lei-fan-qin-lin-and-bin-hu-presented-at-ieee-icra-2025",
           title: '🤖 Distributed Perception Aware Safe Leader Follower System via Control Barrier Methods with...',
           description: "",
+          section: "News",},{id: "news-toward-embedded-llm-guided-navigation-and-object-detection-for-aerial-robots-with-bin-hu-presented-at-icra-2025-late-breaking-session",
+          title: '🛸 Toward Embedded LLM-Guided Navigation and Object Detection for Aerial Robots with Bin...',
+          description: "",
           section: "News",},{id: "news-a-high-fidelity-3d-simulation-environment-for-multi-aircraft-operations-with-timothy-bautista-fengyu-wang-and-colleagues-accepted-at-aiaa-scitech-2026",
           title: '📄 A High-Fidelity 3D Simulation Environment for Multi-Aircraft Operations with Timothy Bautista, Fengyu...',
           description: "",
@@ -51,7 +54,17 @@ ninja.data = [{
           section: "News",},{id: "news-new-preprint-distributed-3d-leader-follower-formation-control-with-field-of-view-safety-via-control-barrier-functions-with-immanuel-r-santjoko-miao-pan-and-bin-hu-is-now-on-arxiv",
           title: '📄 New preprint: Distributed 3D Leader-Follower Formation Control with Field-of-View Safety via Control...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-perception-aware-leader-follower-control",
+          title: 'Perception-Aware Leader-Follower Control',
+          description: "Distributed Perception Aware Safe Leader Follower System via Control Barrier Methods",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/icra2025_cbf/";
+            },},{id: "projects-llm-guided-aerial-navigation",
+          title: 'LLM-Guided Aerial Navigation',
+          description: "Toward Embedded LLM-Guided Navigation and Object Detection for Aerial Robots",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/icra2025_llm/";
+            },},{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
